26e9c43f4535d2a44ee662c016d61bf9f570c9e8
Nikola Pavlović <nikolap994@gmail.com>
2022-05-29 17:14:31 +0200

    Move Crypto params to config.json

--- test/security/index.js
+++ test/security/index.js
@@ -3,20 +3,7 @@ const Monitor = new Status("AES.json");
 
 Monitor.scriptStart("AES_SETUP");
 
-const config = {
-  AES: {
-    ENC_KEY: "bf3c199c2470cb477d907b1e0917c17b",
-    IV: "5183666c72eec9e4",
-  },
-  DES: {
-    ENC_KEY: "bf3c199c2470cb477d907b1e0917c17b",
-    IV: "5183666c72eec9e4",
-  },
-  TRIPLE_DES: {
-    ENC_KEY: "123456000000000000000000",
-    IV: "12345678",
-  },
-};
+const config = require("../../config.json");
 
 const AES = require("../../helper/security/aes");
 
