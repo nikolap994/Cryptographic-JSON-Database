7acea39dedf009b4a9e9174b04b8cb62591fe942
Nikola Pavlović <nikolap994@gmail.com>
2022-06-01 00:50:58 +0200

    Update test script

--- test/json/index.js
+++ test/json/index.js
@@ -1,27 +1,52 @@
 const SecureJson = require("../../helper/json");
-const DB = new SecureJson();
+const DB = new SecureJson(
+  "test.json",
+  "utf-8",
+  "aes",
+  "bf3c199c2470cb477d907b1e0917c17b",
+  "5183666c72eec9e4"
+);
 
-// DB.read().then((data) => console.log(data));
+DB.write(
+  JSON.stringify([
+    {
+      userId: 2,
+      id: 122,
+      title: "delectus aut autem",
+      completed: false,
+    },
+    {
+      userId: 3,
+      id: 33,
+      title: "delectus aut autem",
+      completed: false,
+    },
+    {
+      userId: 4,
+      id: 22,
+      title: "delectus aut autem",
+      completed: false,
+    },
+  ])
+);
 
-// DB.remove(['userId', 125125125]);
+DB.read().then((data) => console.log(data));
 
-// DB.find(['title', 'delectus aut autem']).then((data) => console.log(data));
+DB.remove(["userId", 2]);
 
-// DB.update(
-//     ['userId', 1241251],
-//     [['completed', false], ['title', 'delectus']]
-// );
+DB.find(["title", "delectus aut autem"]).then((data) => console.log(data));
 
-// DB.append({
-// 	"userId": 221213121231231231,
-// 	"id": 122,
-// 	"title": "delectus aut autem",
-// 	"completed": false
-// });
+DB.update(
+  ["userId", 3],
+  [
+    ["completed", false],
+    ["title", "delectus"],
+  ]
+);
 
-// DB.write(JSON.stringify({
-// 	"userId": 2,
-// 	"id": 122,
-// 	"title": "delectus aut autem",
-// 	"completed": false
-// }));
+DB.append({
+  userId: 221213121231231231,
+  id: 122,
+  title: "delectus aut autem",
+  completed: false,
+});
